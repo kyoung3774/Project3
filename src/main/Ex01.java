ff3e27050c80714103978817ce5e4d5677be10a5
BaeJooHwan <kyoung3774@naver.com>
2024-06-24 16:54:52 +0900

    2024.06.24

---
+++ src/main/Ex01.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = -20;
+		
+		//부호를 그대로 유지
+		System.out.println(+num1);
+		System.out.println(+num2);
+		System.out.println();
+		
+		//부호를 반대로 바꾸기
+		System.out.println(-num1);
+		System.out.println(-num2);
+		System.out.println();
+		
+		//부호연산자는 값 자체를 바꾸지 않고, 값을 카피해서 사용함
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+
+}
---
+++ src/quiz/Quiz01.java
@@ -0,0 +1,21 @@
+package quiz;
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+		
+		int num = -10; // -10
+		int result = +num; // -10
+			
+		int num2 = -20;	// -20
+		int result2 = -num2; // 20
+		
+		System.out.println("num: " + num);
+		System.out.println("result: " + result);
+		System.out.println();
+		
+		System.out.println("num2: " + num2);
+		System.out.println("result2: " + result2);
+	}
+
+}
